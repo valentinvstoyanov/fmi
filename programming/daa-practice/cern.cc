7f774ba6299841f3f123594b989e68d7f3ed2f73
valentinvstoyanov <valentinstoqnov98@gmail.com>
2019-02-23 18:59:45 +0200

    Cern task

---
+++ programming/daa-practice/cern.cc
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+const int MAX_SIZE = 1000;
+const int MAX_NUMBER_LEN = 8;
+
+class Number {
+  public:
+	char arr[MAX_NUMBER_LEN];
+
+	bool operator<(const Number& other) const {
+		const char* this_ptr = arr;
+		const char* other_ptr = other.arr;
+		while (*this_ptr && *this_ptr == *other_ptr) {
+			++this_ptr;
+			++other_ptr;
+		}
+		return *this_ptr - *other_ptr < 0;	
+	}
+};
+
+void insertSorted(Number* arr, int size, Number num) {
+	arr[size] = num;
+	for (int i = size - 1; i >= 0; --i) {
+		if (arr[i] < num) {
+			break;
+		} else {
+			swap(arr[i], arr[i + 1]);
+		}	
+	}
+}
+
+int main() {
+	int n;
+	cin >> n;
+
+	Number arr[MAX_SIZE];
+	Number num;
+	for (int i = 0; i < n; ++i) {
+		cin >> num.arr;
+		insertSorted(arr, i, num);
+	}
+
+	for (int i = 0; i < n; ++i) {
+		cout << arr[i].arr << '\n';
+	}
+
+	return 0;
+}
