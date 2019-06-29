703052ef3926ce8cfb9a11594136c116255ee122
valentinvstoyanov <valentinstoqnov98@gmail.com>
2019-06-29 18:51:13 +0300

    Add more prolog predicates

--- programming/prolog/exam_prep.pl
+++ programming/prolog/exam_prep.pl
@@ -98,3 +98,85 @@ add_vertex(X, Vs, [X|Vs]) :- not(member(X, Vs)).
 %extract vertices
 vertices([], []).
 vertices([[X, Y]|Es], Vs) :- vertices(Es, V1s), add_vertex(X, V1s, XV1s), add_vertex(Y, XV1s, Vs).
+
+%simple path
+simple_path(Es, X, Y, P) :- sp(Es, X, Y, [], P).
+sp(_, Y, Y, Visited, Visited).
+sp(Es, W, Y, Visited, P) :- W \= Y, member([W, X], Es), not(member(X, Visited)), appnd(Visited, [X], XVisited), sp(Es, X, Y, XVisited, P). 
+
+%is connected
+is_connected([Vs, Es]) :- not((member(X, Vs), member(Y, Vs), not(simple_path(Es, X, Y, _)))).
+
+%is complete
+is_complete([Vs, Es]) :- not((member(X, Vs), member(Y, Vs), not(member([X, Y], Es)))).
+
+%is undirected
+is_undirected(Es) :- not((member([X, Y], Es), not(member([Y, X], Es)))).
+
+%is directed
+is_directed(Es) :- not(is_undirected(Es)).
+
+%is multi graph
+is_multi(Es) :- remv(E, Es, EEs), not(member(E, EEs)).
+
+%has cycle
+has_cycle(Es) :- member([X, Y], Es), X \= Y, simple_path(Es, Y, X, P), length(P, N), N > 2.
+
+%clique
+clique([Vs, Es], Cs) :- subset(Cs, Vs), is_complete([Cs, Es]).
+
+%max clique
+max_clique([Vs, Es], Cs, K) :- clique([Vs, Es], Cs), length(Cs, K), not((clique([Vs, Es], C1s), length(C1s, K1), K1 > K)).
+
+%is cut vertex
+is_cut([Vs, Es], X) :- remv(X, Vs, XVs), not(is_connected([XVs, Es])).
+
+%natural numbers generator
+nat(0).
+nat(N) :- nat(N1), N is N1 + 1.
+
+%pair of natural numbers
+nat_pair(A, B) :- nat(N), between(0, N, A), B is N - A.
+
+%integer generator
+int(Z) :- nat(N), switch_sign(N, Z).
+switch_sign(0, 0).
+switch_sign(A, B) :- A > 0, (B is A; B is -A).
+
+%between - generate number in [A, B]
+between(A, B, A) :- A =< B. 
+between(A, B, C) :- A < B, A1 is A + 1, between(A1, B, C).
+
+%range - generate list of numbers in [A, B]
+range(A, A, [A]).
+range(A, B, [A|Xs]) :- A < B, A1 is A + 1, range(A1, B, Xs).
+
+%generate K numbers with sum S
+gen_ks(1, S, [S]).
+gen_ks(K, S, [Xi|Xs]) :- K > 1, between(0, S, Xi), SXi is S - Xi, K1 is K - 1, gen_ks(K1, SXi, Xs).
+
+%generate all finite lists of natural numbers
+gen_all_nat_seq([]).
+gen_all_nat_seq(Xs) :- nat(N), between(1, N, K), S is N - K, gen_ks(K, S, Xs).
+
+%flatten
+flatten([], []).
+flatten(X, [X]) :- not(is_list(X)).
+flatten([X|Xs], Ys) :- flatten(X, FX), flatten(Xs, FXs), append(FX, FXs, Ys).
+
+%split
+splt([], []).
+splt(Xs, [Y|Ys]) :- append(Y, Zs, Xs), Y \= [], splt(Zs, Ys).
+
+%count
+cnt([], _, 0).
+cnt([X|Xs], X, N) :- cnt(Xs, X, N1), N is N1 + 1.
+cnt([X|Xs], Y, N) :- X \= Y, cnt(Xs, Y, N).
+
+%fibonacci
+fib(N) :- fib(N, _).
+fib(0, 1).
+fib(X, Y) :- fib(W, X), Y is W + X.
+
+%most frequent(least frequent is the same, just change the sign).
+mf(X, N, Xs) :- member(X, Xs), cnt(Xs, X, N), not((member(X1, Xs), cnt(Xs, X1, N1), N < N1)).
