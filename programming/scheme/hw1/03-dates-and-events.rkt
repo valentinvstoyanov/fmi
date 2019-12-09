b9b61b0b0787d6103f6c3c361cb95f5a16ca7bbb
valentinvstoyanov <valentinstoqnov98@gmail.com>
2019-12-09 18:31:29 +0200

    Add more tests for dates and events task hw1 scheme

--- programming/scheme/hw1/03-dates-and-events.rkt
+++ programming/scheme/hw1/03-dates-and-events.rkt
@@ -11,7 +11,7 @@
     ((1 3 5 7 8 10 12) #t)
     (else #f)))
 
-(define (30? m) (not (31? m)))
+(define (30? m) (and (not (31? m)) (not (= m 2))))
 
 (define (make-date d m y)
   (let ((date (list ds d m y)))
@@ -131,9 +131,30 @@
               (test-case "should return #f for January" (check-false (30? 1)))
               (test-case "should return #f for May" (check-false (30? 5)))
               (test-case "should return #t for November" (check-true (30? 11)))
-              (test-case "should return #t for February" (check-true (30? 2)))
+              (test-case "should return #f for February" (check-false (30? 2)))
               (test-case "should return #t for April" (check-true (30? 4)))))
 (run-tests 30-tests 'verbose)
+
+(define day-tests
+  (test-suite "day tests"
+              (test-case "should return 21 for 21.12.2019" (check-eq? 21 (day (make-date 21 12 2019))))
+              (test-case "should return 1 for 1.11.2020" (check-eq? 1 (day (make-date 1 11 2020))))
+              (test-case "should return 31 for 31.10.2018" (check-eq? 31 (day (make-date 31 10 2018))))))
+(run-tests day-tests 'verbose)
+
+(define month-tests
+  (test-suite "month tests"
+              (test-case "should return 12 for 21.12.2019" (check-eq? 12 (month (make-date 21 12 2019))))
+              (test-case "should return 11 for 1.11.2020" (check-eq? 11 (month (make-date 1 11 2020))))
+              (test-case "should return 10 for 31.10.2018" (check-eq? 10 (month (make-date 31 10 2018))))))
+(run-tests month-tests 'verbose)
+
+(define year-tests
+  (test-suite "year tests"
+              (test-case "should return 2019 for 21.12.2019" (check-eq? 2019 (year (make-date 21 12 2019))))
+              (test-case "should return 2020 for 1.11.2020" (check-eq? 2020 (year (make-date 1 11 2020))))
+              (test-case "should return 2018 for 31.10.2018" (check-eq? 2018 (year (make-date 31 10 2018))))))
+(run-tests year-tests 'verbose)
 
 (define date-tests
   (test-suite "date? tests"
@@ -144,7 +165,64 @@
               (test-case "should return #f for 31.9.2019" (check-false (date? (make-date 31 9 2019))))
               (test-case "should return #f for 5.13.2019" (check-false (date? (make-date 5 13 2019))))))
 (run-tests date-tests 'verbose)
-              
+
+(define date->string-tests
+  (test-suite "date->string tests"
+              (test-case "should return \"21.11.2019\" for 21.11.2019" (check-equal? "21.11.2019" (date->string (make-date 21 11 2019))))
+              (test-case "should return \"1.2.-1239\" for 1.2.-1239" (check-equal? "1.2.-1239" (date->string (make-date 1 2 -1239))))))
+(run-tests date->string-tests 'verbose)
+
+(define next-day-tests
+  (test-suite "next-day tests"
+              (test-case "should return 22.11.2019 for 21.11.2019" (check-equal? "22.11.2019" (date->string (next-day (make-date 21 11 2019)))))
+              (test-case "should return 1.12.2019 for 30.11.2019" (check-equal? "1.12.2019" (date->string (next-day (make-date 30 11 2019)))))
+              (test-case "should return 1.1.2020 for 31.12.2019" (check-equal? "1.1.2020" (date->string (next-day (make-date 31 12 2019)))))))
+(run-tests next-day-tests 'verbose)
+         
+(define date<-tests
+  (test-suite "date< tests"
+              (test-case "should return #t for 21.11.2019 < 1.1.2020" (check-true (date< (make-date 21 11 2019) (make-date 1 1 2020))))
+              (test-case "should return #f for 21.11.2019 < 1.1.2019" (check-false (date< (make-date 21 11 2019) (make-date 1 1 2019))))
+              (test-case "should return #f when the dates are equal" (check-false (date< (make-date 21 11 2010) (make-date 21 11 2010))))))
+(run-tests date<-tests 'verbose)
+
+(define weekday-tests
+  (test-suite "weekday tests"
+              (test-case "should return 'Thursday for 21.11.2019" (check-eqv? 'Thursday (weekday (make-date 21 11 2019))))
+              (test-case "should return 'Friday for 22.11.2019" (check-eqv? 'Friday (weekday (make-date 22 11 2019))))
+              (test-case "should return 'Monday for 9.11.2019" (check-eqv? 'Monday (weekday (make-date 9 12 2019))))))
+(run-tests weekday-tests 'verbose)
+
+(define next-weekday-tests
+  (test-suite "next-weekday tests"
+              (test-case "should return 28.11.2019 for 'Thursday and 21.11.2019" (check-equal? "28.11.2019" (date->string (next-weekday 'Thursday (make-date 21 11 2019)))))
+              (test-case "should return 26.11.2019 for 'Tuesday and 21.11.2019" (check-equal? "26.11.2019" (date->string (next-weekday 'Tuesday (make-date 21 11 2019)))))))
+(run-tests next-weekday-tests 'verbose)
+
+
+(define events
+  (list (cons (make-date 27 11 2019) "Първа лекция за Хаскел")
+                      (cons (make-date 27 11 2019) "Спират водата в Младост")
+                      (cons (make-date 28 11 2019) "Спират водата в Лозенец")))
+(define 27-events (list "Първа лекция за Хаскел" "Спират водата в Младост"))
+(define 28-events (list  "Спират водата в Лозенец"))
+(define events-for-day-tests
+  (test-suite "events-for-day tests"
+              (test-case "should return all events for 27.11.2019" (check-equal? 27-events (map cdr (events-for-day (make-date 27 11 2019) events))))
+              (test-case "should return all events for 28.11.2019" (check-equal? 28-events (map cdr (events-for-day (make-date 28 11 2019) events))))))
+(run-tests events-for-day-tests 'verbose)
+
+
+(define cal
+  (calendar (list (cons (make-date 27 11 2019) "Първа лекция за Хаскел")
+                (cons (make-date 25 12 2019) "Коледа")
+                (cons (make-date 27 11 2019) "Спират водата в Младост")
+                (cons (make-date 23 3 2018) "Концерт на Лепа Брена"))))
+(define cal-res '(("Концерт на Лепа Брена") ("Първа лекция за Хаскел" "Спират водата в Младост") ("Коледа")))
+(define calendar-tests
+  (test-suite "calendar tests"
+              (test-case "should return assoc list with date as a key and list of the events in ascending order" (check-equal? cal-res (map cdr cal)))))
+(run-tests calendar-tests 'verbose)
 
   
 
