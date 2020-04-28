bfb53feccb37d3e11a528cd957d113625e1ad11a
valentinvstoyanov <valentinstoqnov98@gmail.com>
2020-04-28 17:40:18 +0300

    Restructure repo

--- README.md
+++ README.md
@@ -1,15 +1,1 @@
-# FMI
-
-Part of the work I've done during my bachelor degree in [Faculty of Mathematics and Informatics(FMI)](https://www.fmi.uni-sofia.bg/en) of [Sofia University](https://www.uni-sofia.bg/eng)
-
-## Content
-* [Math courses](https://github.com/valentinstoqnov/fmi-education/tree/master/math)
-  * [Abstract algebra](https://github.com/valentinstoqnov/fmi-education/tree/master/math/abstract-algebra)
-  * [Discrete structures](https://github.com/valentinstoqnov/fmi-education/tree/master/math/dicrete-structures)
-  
-* [Programming courses](https://github.com/valentinstoqnov/fmi-education/tree/master/programming)
-  * [Introduction to programming with C++](https://github.com/valentinstoqnov/fmi-education/tree/master/programming/introduction-to-programming-cpp)
-  * [Object oriented programming with C++](https://github.com/valentinstoqnov/fmi-education/tree/master/programming/oop-cpp)
-  
-## Acknowledgement
-[Faculty of Mathematics and Informatics(FMI)](https://www.fmi.uni-sofia.bg/en) of [Sofia University](https://www.uni-sofia.bg/eng)
+University stuff
--- ai/8-puzzle-A*/main.cpp
+++ ai/8-puzzle-A*/main.cpp
@@ -4,23 +4,25 @@
 template <size_t SIZE>
 void printSolution(const std::vector<Board<SIZE>>&);
 
-int main() {
-  const auto kBoardSize = 3;
+template<size_t SIZE>
+typename Puzzle8Solver<SIZE>::Cost hammingHeuristic(const Board<SIZE>& from, const Board<SIZE>& to) {
+  auto diff_counter = 0;
+  for (size_t i = 0; i < from.size(); ++i)
+    for (size_t j = 0; j < from.size(); ++j)
+      if (from(i, j) >= 0 && from(i, j) != to(i, j)) ++diff_counter;
 
-  const auto fixedSrc = Board<kBoardSize>({1, 8, 2, -1, 4, 3, 7, 6, 5}, {1, 0});
+  return diff_counter;
+};
+
+int main() {
+  constexpr auto kBoardSize = 3;
+
+  const auto fixedSrc1 = Board<kBoardSize>({1, 2, 3, 4, -1, 6, 7, 5, 8}, {1, 1});
+  const auto fixedSrc2 = Board<kBoardSize>({1, 8, 2, -1, 4, 3, 7, 6, 5}, {1, 0});
   const auto randomSrc = BoardBuilder<kBoardSize>::createRandomBoard({1, 2, 3, 4, 5, 6, 7, 8});
   const auto dest = Board<kBoardSize>({1, 2, 3, 4, 5, 6, 7, 8, -1}, {2, 2});
 
-  const auto hammingHeuristic = [](const Board<kBoardSize>& from, const Board<kBoardSize>& to) -> Puzzle8Solver<kBoardSize>::Cost {
-    auto diff_counter = 0;
-    for (size_t i = 0; i < from.size(); ++i)
-      for (size_t j = 0; j < from.size(); ++j)
-        if (from(i, j) >= 0 && from(i, j) != to(i, j)) ++diff_counter;
-
-    return diff_counter;
-  };
-
-  Puzzle8Solver<kBoardSize> solver(hammingHeuristic);
+  Puzzle8Solver<kBoardSize> solver(hammingHeuristic<kBoardSize>);
   solver.solve(randomSrc, dest);
 
   if (solver.hasSolution()) printSolution(solver.solution());
